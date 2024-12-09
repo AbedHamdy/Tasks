52876e8705b073e841dda22bafd73a5b9584ad5f
abed <abdoezzat960@gmail.com>
2024-12-09 05:59:10 -0800

    first task compelet

---
+++ tasks ses2/index.php
@@ -0,0 +1,209 @@
+<?php
+
+
+// 1
+    // function result($num1 , $num2 , $num3)
+    // {
+    //     echo ($num1+$num2)*$num3;
+    // }
+
+    // result(2,2,1);
+
+// 2
+    // function size($height)
+    // {
+    //     echo $height *5*10;
+    // }
+
+    // size(4);
+
+// 3
+    // function seconds ($hours)
+    // {
+    //     echo $hours*60*60 . " seconds";
+    // }
+
+    // seconds(2);
+
+// 4
+    // function AreaOfTriangle($base , $height)
+    // {
+    //     echo 1/2 * $base * $height;
+    // }
+
+    // area_of_triangle(12,3);
+
+// 5
+    // function AgeInDays($years)
+    // {
+    //     echo $years * 365.25;
+    // }
+
+    // AgeInDays(4);
+
+// 6
+    // $text = "EraaSoft Learn by practice";
+    // echo strlen($text);
+    // 7 
+        // $new_text = str_replace(" ","",$text);
+        // echo strlen($new_text);
+    // 8
+        // echo str_word_count($text);
+    // 9
+        // $res = strpos($text,"by");
+        // if($res != 0)
+        // {
+        //     echo "exists";
+        // }
+        // else 
+        // {
+        //     echo "not found";
+        // }
+    // 10
+        // $word = "EraaSoft";
+        // $pos_text = strpos($text , $word);
+        // $len_text = strlen($word);
+        // $print = substr($text , $pos_text , $len_text);
+        // echo $print;
+    // 11
+        // $new_text = str_replace("by" , "" , $text);
+        // echo $text . "<br>" . $new_text;
+
+// 12 
+        // $string_one = "Eraa"; 
+        // $string_two = "Soft";
+        // $full_string = $string_one . $string_two;
+        // echo $full_string;
+    // 13
+        // if($full_string == "EraaSoft")
+        // {
+        //     echo "equal";
+        // }
+        // else 
+        // {
+        //     echo "not equal";
+        // }
+
+// 14
+    // $text = "ErraSoft";
+    // echo wordwrap($text ,2 ,"/", true);
+
+// 15
+    // function num($num)
+    // {
+    //     if($num %2 == 0)
+    //     {
+    //         echo "even";
+    //     }
+    //     else 
+    //     {
+    //         echo "odd";
+    //     }
+    // }
+
+    // num(-10);
+
+// 16
+    // function text($text)
+    // {
+    //     $len_text = strlen($text);
+    //     if($len_text % 2 == 0)
+    //     {
+    //         echo "even";
+    //     }
+    //     else 
+    //     {
+    //         echo "odd";
+    //     }
+    // }
+
+    // text("asdacdacac");
+
+// 17
+    // $description = "no pain , no gain ";
+    // $num_gain = strpos($description,"gain");
+    // $num_peen = strpos($description,"peen");
+    // if($num_gain >= 0 || $num_peen >= 0)
+    // {
+    //     echo "success word";
+    // }
+    // else 
+    // {
+    //     echo "wrong word";
+    // }
+
+// 18
+    // function num($num)
+    // {
+    //     if($num == 0)
+    //     {
+    //         echo "1";
+    //     }
+    //     else if($num == 1)
+    //     {
+    //         echo "0";
+    //     }
+    // }
+
+    // num(1);
+    // num(0);
+
+// 19
+    // function text($text)
+    // {
+    //     $new_text = substr($text, -1);
+    //     if($new_text == "s")
+    //     {
+    //         echo "plural";
+    //     }
+    //     else 
+    //     {
+    //         echo "Singular";
+    //     }
+    // }
+
+    // text("asdasds");
+
+// 20
+    // function calculate($num1 , $op , $num2)
+    // {
+    //     if($op == "+")
+    //     {
+    //         echo $num1 + $num2;
+    //     }
+    //     else if($op == "-")
+    //     {
+    //         echo $num1 - $num2;
+    //     }
+    //     else if($op == "*")
+    //     {
+    //         echo $num1 * $num2;
+    //     }
+    //     else if($op == "/")
+    //     {
+    //         echo $num1 / $num2;
+    //     }
+    //     else if($op == "^")
+    //     {
+    //         echo $num1 * $num1;
+    //     }
+    //     else if($op == "%")
+    //     {
+    //         echo $num1 % $num2;
+    //     }
+    //     else 
+    //     {
+    //         echo "invaled";
+    //     }
+    // }
+
+    // calculate(3 ,"^" ,2);
+
+
+
+
+
+
+
+
+?>
